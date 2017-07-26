7ddd8d920615d2736dc7a71b8accc82ab360d0e7
erinworth <eeworth@gmail.com>
2017-07-26 10:37:53 -0700

    working on checkpoint 5

---
+++ app/scripts/controllers/CookiesCtrl.js
@@ -0,0 +1,20 @@
+(function() {
+  function BlocChatCookies($cookies) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+      // Do something to allow users to set their username
+      $uibModal.open({
+      //Modal config object properties
+          templateUrl: '/templates/username.html',
+          size: 'sm',
+          controller: 'UserCtrl',
+          controllerAs: 'userctrl'
+      })
+    }
+      console.log(currentUser);
+  }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies', BlocChatCookies]);
+})();
---
+++ app/scripts/controllers/UserCtrl.js
@@ -0,0 +1,18 @@
+(function(){
+    function UserCtrl($cookies, $uibModalInstance){
+      var UserCtrl = this;
+
+      this.createUsername = function(){
+          if(this.username){
+            $cookies.put('blocChatCurrentUser', userCtrl.username);
+            $uibModalInstance.close();
+        } else{
+            alert("You must create a username to send messages!");
+        }
+      };
+  };
+
+  angular
+      .module('blocChat')
+      .controller('UserCtrl', ['$cookies', '$uibModalInstance', UserCtrl]);
+})();
